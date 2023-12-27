5873218695a1b53f3568de01abd83bc59920119e
Seo <apej88@gmail.com>
2023-12-27 16:39:42 +0900

    frame

--- album_app/urls.py
+++ album_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path( '', views.index, name='index'),
+    path('exhibition/', views.exhibition, name='exhibition'),
 ]
--- album_app/views.py
+++ album_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'album_app/index.html')
+
+def exhibition(request):
+    return render(request, 'exhibition.html')
