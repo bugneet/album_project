2c126db2c0d71b6d5ff4021f9f7e9468920521fc
bugneet <bugneet.dev@gmail.com>
2023-12-28 13:07:15 +0900

    mypage_album_frame

--- album_app/urls.py
+++ album_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path( '', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('/mypage_album', views.mypage_album, name='mypage_album'),
 ]
--- album_app/views.py
+++ album_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'album_app/index.html')
+
+def mypage_album(request):
+    return render(request, 'album_app/mypage_album.html')
